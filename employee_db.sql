5338e4d58022817aa3ba6d10e3f49ca0ead1636b
JD <janelle.m.deane@gmail.com>
2020-10-13 14:58:03 -0700

    redid database

--- employee_db.sql
+++ employee_db.sql
@@ -4,18 +4,6 @@ CREATE DATABASE employee_db;
 
 USE employee_db;
 
-CREATE TABLE employeeList (
-  id INT NOT NULL AUTO_INCREMENT,
-  title VARCHAR(30) NOT NULL,
-  first_name VARCHAR(30) NOT NULL
-  last_name VARCHAR(30) NOT NULL
-  salary INT NULL,
-  PRIMARY KEY (id)
-);
-
-INSERT INTO employeeList (department, title, first_name, last_name, salary)
-VALUES ("Test Employee", "test manager", "test bob", "test bologane" 1000);
-
 CREATE TABLE departmentList (
 id INT PRIMARY KEY AUTO_INCREMENT,
 department_name VARCHAR(30),
@@ -23,18 +11,24 @@ PRIMARY KEY (id),
 );
 
 CREATE TABLE roleList (
-id - INT PRIMARY KEY
-title - VARCHAR(30) NOT NULL
-salary - DECIMAL NOT NULL
-department_id INT NOT NULL
-PRIMARY KEY (id)
+id INT PRIMARY KEY,
+title VARCHAR(30) NOT NULL,
+salary DECIMAL NOT NULL,
+department_id INT NOT NULL,
+PRIMARY KEY (id),
 FOREIGN KEY (department_id) REFERENCES departmentList(id)
 ); 
 
 CREATE TABLE employeeList(
-id - INT PRIMARY KEY
-first_name - VARCHAR(30) to hold employee first name
-last_name - VARCHAR(30) to hold employee last name
-role_id - INT to hold reference to role employee has
-manager_id - INT
+id INT PRIMARY KEY,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INT,
+manager_id INT,
+FOREIGN KEY (role_id) REFERENCES role(id),
+FOREIGN KEY (manager_id) REFERENCES employeeList(id),
+PRIMARY KEY (id)
 );
+
+INSERT INTO employeeList (department, title, first_name, last_name, salary)
+VALUES ("Test Employee", "test manager", "test bob", "test bologane" 1000);
